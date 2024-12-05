20ad8386d69eda970d7edcd9be1a6985f140e63e
=motohrr <cherreramoto555@gmail.com>
2024-12-05 12:13:31 -0600

    Added form taost after success

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,5 +1,17 @@
 import IMask from 'imask';
 import JustValidate from 'just-validate';
+import Toastify from 'toastify-js'
+
+/* Toastify setup */
+const showToast = (message, type = 'success') => {
+  Toastify({
+    text: message,
+    duration: 3000,
+    gravity: 'top',
+    position: 'right',
+    backgroundColor: type === 'success' ? 'green' : 'red',
+  }).showToast();
+};
 
 /* Validator for contact form */
 const validator = new JustValidate('#contact-form');
@@ -7,99 +19,119 @@ validator
   .addField('#name', [
     {
       rule: 'required',
-      errorMessage: 'El nombre es obligatorio'
+      errorMessage: 'El nombre es obligatorio',
     },
     {
       rule: 'minLength',
       value: 3,
-      errorMessage: 'El nombre debe tener al menos 3 caracteres'
-    }
+      errorMessage: 'El nombre debe tener al menos 3 caracteres',
+    },
   ])
   .addField('#email', [
     {
       rule: 'required',
-      errorMessage: 'El correo es obligatorio'
+      errorMessage: 'El correo es obligatorio',
     },
     {
       rule: 'email',
-      errorMessage: 'Debe ser un correo válido'
-    }
+      errorMessage: 'Debe ser un correo válido',
+    },
   ])
   .addField('#phone', [
     {
       rule: 'required',
-      errorMessage: 'El teléfono es obligatorio'
+      errorMessage: 'El teléfono es obligatorio',
     },
     {
       rule: 'minLength',
       value: 14,
-      errorMessage: 'El teléfono debe tener al menos 14 caracteres'
-    }
+      errorMessage: 'El teléfono debe tener al menos 14 caracteres',
+    },
   ])
   .addField('#message', [
     {
       rule: 'required',
-      errorMessage: 'El mensaje es obligatorio'
+      errorMessage: 'El mensaje es obligatorio',
     },
     {
       rule: 'minLength',
       value: 5,
-      errorMessage: 'El mensaje debe tener al menos 5 caracteres'
-    }
-  ]);
+      errorMessage: 'El mensaje debe tener al menos 5 caracteres',
+    },
+  ])
+  .onSuccess((event) => {
+    event.preventDefault();
 
-/* Schedule a appt form */
+    const form = event.target;
+
+    setTimeout(() => {
+      showToast('Formulario enviado correctamente!');
+      form.reset();
+    }, 500);
+  });
+
+/* Validator for appointment form */
 const validatorAppt = new JustValidate('#appt-form');
 validatorAppt
   .addField('#appt-name', [
     {
       rule: 'required',
-      errorMessage: 'El nombre es obligatorio'
+      errorMessage: 'El nombre es obligatorio',
     },
     {
       rule: 'minLength',
       value: 3,
-      errorMessage: 'El nombre debe tener al menos 3 caracteres'
-    }
+      errorMessage: 'El nombre debe tener al menos 3 caracteres',
+    },
   ])
   .addField('#appt-email', [
     {
       rule: 'required',
-      errorMessage: 'El correo es obligatorio'
+      errorMessage: 'El correo es obligatorio',
     },
     {
       rule: 'email',
-      errorMessage: 'Debe ser un correo válido'
-    }
+      errorMessage: 'Debe ser un correo válido',
+    },
   ])
   .addField('#appt-phone', [
     {
       rule: 'required',
-      errorMessage: 'El teléfono es obligatorio'
+      errorMessage: 'El teléfono es obligatorio',
     },
     {
       rule: 'minLength',
       value: 14,
-      errorMessage: 'El teléfono debe tener al menos 14 caracteres'
-    }
+      errorMessage: 'El teléfono debe tener al menos 14 caracteres',
+    },
   ])
   .addField('#appt-message', [
     {
       rule: 'required',
-      errorMessage: 'El mensaje es obligatorio'
+      errorMessage: 'El mensaje es obligatorio',
     },
     {
       rule: 'minLength',
       value: 5,
-      errorMessage: 'El mensaje debe tener al menos 5 caracteres'
-    }
-  ]);
+      errorMessage: 'El mensaje debe tener al menos 5 caracteres',
+    },
+  ])
+  .onSuccess((event) => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    setTimeout(() => {
+      showToast('Solicitud enviada correctamente!');
+      form.reset();
+    }, 500);
+  });
 
 /* Input mask implementation */
 const phoneInput = document.querySelector('.phone');
 if (phoneInput) {
   IMask(phoneInput, {
-    mask: '(000) 0000-0000'
+    mask: '(000) 0000-0000',
   });
 }
 
@@ -115,7 +147,7 @@ const observer = new IntersectionObserver(
     });
   },
   {
-    threshold: 0.1
+    threshold: 0.1,
   }
 );
 
