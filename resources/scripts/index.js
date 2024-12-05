6c32a25e03cb17fa6f94536530683fc50b3257f4
=motohrr <cherreramoto555@gmail.com>
2024-12-05 12:02:19 -0600

    Added modal window

--- resources/scripts/index.js
+++ resources/scripts/index.js
@@ -1,8 +1,8 @@
 import IMask from 'imask';
 import JustValidate from 'just-validate';
 
+/* Validator for contact form */
 const validator = new JustValidate('#contact-form');
-
 validator
   .addField('#name', [
     {
@@ -48,6 +48,53 @@ validator
     }
   ]);
 
+/* Schedule a appt form */
+const validatorAppt = new JustValidate('#appt-form');
+validatorAppt
+  .addField('#appt-name', [
+    {
+      rule: 'required',
+      errorMessage: 'El nombre es obligatorio'
+    },
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'El nombre debe tener al menos 3 caracteres'
+    }
+  ])
+  .addField('#appt-email', [
+    {
+      rule: 'required',
+      errorMessage: 'El correo es obligatorio'
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Debe ser un correo válido'
+    }
+  ])
+  .addField('#appt-phone', [
+    {
+      rule: 'required',
+      errorMessage: 'El teléfono es obligatorio'
+    },
+    {
+      rule: 'minLength',
+      value: 14,
+      errorMessage: 'El teléfono debe tener al menos 14 caracteres'
+    }
+  ])
+  .addField('#appt-message', [
+    {
+      rule: 'required',
+      errorMessage: 'El mensaje es obligatorio'
+    },
+    {
+      rule: 'minLength',
+      value: 5,
+      errorMessage: 'El mensaje debe tener al menos 5 caracteres'
+    }
+  ]);
+
 /* Input mask implementation */
 const phoneInput = document.querySelector('.phone');
 if (phoneInput) {
@@ -68,7 +115,7 @@ const observer = new IntersectionObserver(
     });
   },
   {
-    threshold: 0.1,
+    threshold: 0.1
   }
 );
 
