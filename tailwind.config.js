9332d379a98192fae2b364c6e2035e5e5442852b
=motohrr <cherreramoto555@gmail.com>
2024-12-04 20:02:37 -0600

    Added base files and setup css dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      'luxury',
+      {
+        'luxury-light': {
+          primary: '#fde047',
+          secondary: '#e5e7eb',
+          accent: '#F6DCCE',
+          neutral: '#F1E8E2',
+          'base-100': '#f5f5f4',
+          info: '#93c5fd',
+          success: '#a3e635',
+          warning: '#fde047',
+          error: '#ef4444'
+        }
+      }
+    ]
+  }
+};
